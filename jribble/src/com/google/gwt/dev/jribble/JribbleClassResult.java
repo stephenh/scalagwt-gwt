0adbaef96a9465dc7d184070e977d02e4735ad29
Stephen Haberman <stephen@exigencecorp.com>
2011-09-03 14:35:59 -0500

    Add separate jribble project.

---
+++ jribble/src/com/google/gwt/dev/jribble/JribbleClassResult.java
@@ -0,0 +1,16 @@
+package com.google.gwt.dev.jribble;
+
+/** Holds the bytecode + AST for a jribble class. */
+public class JribbleClassResult {
+
+  public final String internalName;
+  public final byte[] byteCode;
+  public final String jribble;
+
+  public JribbleClassResult(String internalName, byte[] byteCode, String jribble) {
+    this.internalName = internalName;
+    this.byteCode = byteCode;
+    this.jribble = jribble;
+  }
+
+}
